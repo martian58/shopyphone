84b456d0c66b50ee16ddbc7da42f3dd91b0cbe55
martian58 <fuadelizade6@gmail.com>
2024-06-01 15:33:15 +0400

    Update cart number

--- cart/cart.py
+++ cart/cart.py
@@ -11,3 +11,17 @@ class Cart():
             cart = self.session['session_key'] = {}
 
         self.cart = cart
+
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 # Create your views here.
 
 def cart_summary(request):
     return render(request, 'cart_summary.html', {})
 
 def cart_add(request):
-    return render(request, 'cart.html', {})
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product)
+
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'quantity: ': cart_quantity})
+        # response = JsonResponse({'Product Name: ': product.name})
+        return response
+    else:
+        pass
 
 
 def cart_delete(request):
