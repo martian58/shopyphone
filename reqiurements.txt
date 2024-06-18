469177061e3bd9abf363630b7eb29dfb2a2b2cf0
martian58 <fuadelizade6@gmail.com>
2024-06-18 19:53:21 +0400

    Fix Requirements

---
+++ reqiurements.txt
@@ -0,0 +1,4 @@
+asgiref==3.8.1
+Django==5.0.6
+pillow==10.3.0
+sqlparse==0.5.0
--- store/views.py
+++ store/views.py
@@ -63,7 +63,7 @@ def register_user(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data['username']
+            username = form.cleaned_data['username'] 
             password = form.cleaned_data['password1']
 
             # Login
